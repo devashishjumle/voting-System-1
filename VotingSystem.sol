5f2b5e7994f935193b3308af16c2437502688c5a
devashishjumle <devashishjumle96@gmail.com>
2025-08-30 23:53:03 +0530

    Update VotingSystem.sol

--- VotingSystem.sol
+++ VotingSystem.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 contract VotingSystem {
     struct Candidate {
         string name;
-        uint voteCount;
+        uint   voteCount;
     }
 
     address public owner;
